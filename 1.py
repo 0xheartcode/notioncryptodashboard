7881bc6c6c0672f7abbe20e3656bdf83d0bb2ca2
0xheartcode <0xheartcode@gmail.com>
2022-11-22 23:05:43 +0100

    update2

---
+++ 1.py
@@ -0,0 +1,198 @@
+{
+ "object": "page",
+ "id": "c3618a00-3eef-4d09-ad29-10c6d7bba8d2",
+ "created_time": "2022-11-19T18:31:00.000Z",
+ "last_edited_time": "2022-11-21T21:29:00.000Z",
+ "created_by": {
+  "object": "user",
+  "id": "0c786039-27be-43d4-91bf-c2946f8396df"
+ },
+ "last_edited_by": {
+  "object": "user",
+  "id": "0c786039-27be-43d4-91bf-c2946f8396df"
+ },
+ "cover": null,
+ "icon": null,
+ "parent": {
+  "type": "database_id",
+  "database_id": "90be3d67-5123-47be-bd31-5dbbca6e7b03"
+ },
+ "archived": false,
+ "properties": {
+  "MCap (Manual)": {
+   "id": "%3FXDa",
+   "type": "number",
+   "number": 289725
+  },
+  "Contract": {
+   "id": "ABiH",
+   "type": "url",
+   "url": "https://etherscan.io/token/0x66d28cb58487a7609877550e1a34691810a6b9fc"
+  },
+  "Status": {
+   "id": "BbUQ",
+   "type": "status",
+   "status": {
+    "id": "3c196fef-a4fe-4331-8d8c-19265cff56ab",
+    "name": "Watching",
+    "color": "default"
+   }
+  },
+  "Discord": {
+   "id": "HAOY",
+   "type": "url",
+   "url": "https://discord.com/invite/XY4BWNY"
+  },
+  "Recommended by": {
+   "id": "Jy%5Ct",
+   "type": "people",
+   "people": []
+  },
+  "Chart": {
+   "id": "PPYd",
+   "type": "url",
+   "url": null
+  },
+  "Created time": {
+   "id": "P%60%5Cz",
+   "type": "created_time",
+   "created_time": "2022-11-19T18:31:00.000Z"
+  },
+  "CG Rank (Manual)": {
+   "id": "Qz%3DV",
+   "type": "number",
+   "number": 2524
+  },
+  "Category": {
+   "id": "R%5CVO",
+   "type": "multi_select",
+   "multi_select": []
+  },
+  "Type": {
+   "id": "SqwR",
+   "type": "multi_select",
+   "multi_select": []
+  },
+  "Twitter": {
+   "id": "URc%5C",
+   "type": "url",
+   "url": "https://twitter.com/koinosnetwork"
+  },
+  "Telegram": {
+   "id": "WWvw",
+   "type": "url",
+   "url": "https://t.me/joinchat/AAAAAEl3upi52_ErMsJavQ"
+  },
+  "CoinGecko": {
+   "id": "XerV",
+   "type": "url",
+   "url": "https://www.coingecko.com/en/coins/koinos"
+  },
+  "Fullname": {
+   "id": "Y%40lE",
+   "type": "rich_text",
+   "rich_text": [
+    {
+     "type": "text",
+     "text": {
+      "content": "Koinos",
+      "link": null
+     },
+     "annotations": {
+      "bold": false,
+      "italic": false,
+      "strikethrough": false,
+      "underline": false,
+      "code": false,
+      "color": "default"
+     },
+     "plain_text": "Koinos",
+     "href": null
+    }
+   ]
+  },
+  "Website": {
+   "id": "%5BZxG",
+   "type": "url",
+   "url": "https://koinos.io/"
+  },
+  "CoingeckoID": {
+   "id": "f%3CG%7B",
+   "type": "rich_text",
+   "rich_text": [
+    {
+     "type": "text",
+     "text": {
+      "content": "koinos",
+      "link": null
+     },
+     "annotations": {
+      "bold": false,
+      "italic": false,
+      "strikethrough": false,
+      "underline": false,
+      "code": false,
+      "color": "default"
+     },
+     "plain_text": "koinos",
+     "href": null
+    }
+   ]
+  },
+  "Description": {
+   "id": "hAQl",
+   "type": "rich_text",
+   "rich_text": []
+  },
+  "Chain": {
+   "id": "nG%3As",
+   "type": "multi_select",
+   "multi_select": [
+    {
+     "id": "2a6bd77f-d0fd-4d10-bda3-5f1f305d767f",
+     "name": "KOINOS",
+     "color": "default"
+    }
+   ]
+  },
+  "Price When Added": {
+   "id": "ra_X",
+   "type": "number",
+   "number": 0.00290315
+  },
+  "Price Now (Manual Update)": {
+   "id": "s%5COT",
+   "type": "number",
+   "number": 0.00290315
+  },
+  "URL": {
+   "id": "%7CtnE",
+   "type": "url",
+   "url": null
+  },
+  "Coin": {
+   "id": "title",
+   "type": "title",
+   "title": [
+    {
+     "type": "text",
+     "text": {
+      "content": "KOIN",
+      "link": null
+     },
+     "annotations": {
+      "bold": false,
+      "italic": false,
+      "strikethrough": false,
+      "underline": false,
+      "code": false,
+      "color": "default"
+     },
+     "plain_text": "KOIN",
+     "href": null
+    }
+   ]
+  }
+ },
+ "url": "https://www.notion.so/KOIN-c3618a003eef4d09ad2910c6d7bba8d2"
+}
